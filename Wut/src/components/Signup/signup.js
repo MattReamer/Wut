3de5aa66ee86c499fb0a4853c72e9637656d8b59
Matthew Reamer <reamer.m.p@gmail.com>
2019-12-15 19:05:00 -0500

    start script

--- Wut/src/components/Signup/signup.js
+++ Wut/src/components/Signup/signup.js
@@ -89,6 +89,9 @@ export default function SignUpExample() {
                 />
             </Form.Group>
             <Button type="submit">Submit form</Button>
+            <p className="forgot-password text-right">
+                Already registered <a href="/login">sign in?</a>
+            </p>
         </Form>
     );
-}
+}                
