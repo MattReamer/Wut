2fb35ec7abbe562b3768f9baa85f292274959eb2
Matthew Reamer <reamer.m.p@gmail.com>
2019-12-16 13:41:36 -0500

    set up connection to mongoDB

--- react-backend/index.js
+++ react-backend/index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const dotenv = require('dotenv')
 const app = express();
-
-
+const mongoose = require('mongoose')
+dotenv.config();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -12,17 +13,19 @@ app.use(bodyParser.json())
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }).then(() => console.log("DB Connected"))
+    .catch(e => console.log(e));
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("mongoDB database connection established successfully")
+})
 // An api endpoint that returns a short list of items
 app.get('/api/getList', (req, res) => {
     res.send({ express: 'OUR EXPRESS BACKEND IS CONNECTED TO REACT BITCHHH' });
     console.log('Sent request');
 });
-app.options('/api/highscore', cors())
-app.post('/api/highscore'), cors(), (req, res) => {
-    res.send('nice')
-    console.log(req.body);
-    console.log('post hit')
-}
 
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
@@ -30,6 +33,7 @@ app.get('*', (req, res) => {
 });
 
 const port = process.env.PORT || 5000;
+
 app.listen(port);
 
 console.log('App is listening on port ' + port);
