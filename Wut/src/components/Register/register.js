11f737f7d4e8a234602718c3d7046567dfc1d3f1
Matthew Reamer <reamer.m.p@gmail.com>
2019-12-16 15:19:43 -0500

    register form send user data to mongodb

--- Wut/src/components/Register/register.js
+++ Wut/src/components/Register/register.js
@@ -7,13 +7,41 @@ import Col from 'react-bootstrap/Col';
 import FormLabel from 'react-bootstrap/FormLabel';
 import Card from 'react-bootstrap/Card'
 import './register.css'
+import axios from 'axios'
 export default class Register extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            name: "",
+            email: "",
+            password: "",
+            password2: "",
+            errors: {}
+        };
 
     }
+
+    onChange = e => {
+        this.setState({ [e.target.id]: e.target.value });
+    };
+
+    onSubmit = e => {
+        e.preventDefault();
+        const newUser = {
+            name: this.state.name,
+            email: this.state.email,
+            password: this.state.password,
+            password2: this.state.password2
+        };
+
+        axios.post("/api/users/register", newUser)
+
+
+
+        console.log(newUser);
+    };
     render() {
+        const { errors } = this.state;
         return (
             <div className="signup-container">
                 <Card className="signup-card">
@@ -21,26 +49,53 @@ export default class Register extends Component {
                         <h2>Register</h2>
                     </Card.Header>
                     <Card.Text>
-                        <Form className="signup-form">
+                        <Form className="signup-form" onSubmit={this.onSubmit}>
+                            <Col>
+                                <FormGroup>
+                                    <FormLabel>Username</FormLabel>
+                                    <Form.Control autoFocus
+                                        onChange={this.onChange}
+                                        value={this.state.name}
+                                        error={errors.name}
+                                        id="name"
+                                        type="text"
+                                        placeholder="Enter email" />
+                                </FormGroup>
+                            </Col>
                             <Col>
                                 <FormGroup>
                                     <FormLabel>Email</FormLabel>
-                                    <Form.Control autoFocus type="email" placeholder="Enter email" />
+                                    <Form.Control autoFocus
+                                        onChange={this.onChange}
+                                        value={this.state.email}
+                                        error={errors.email}
+                                        id="email"
+                                        type="text"
+                                        placeholder="Enter email" />
                                 </FormGroup>
                             </Col>
                             <Col>
                                 <FormGroup>
                                     <Form.Label>Password</Form.Label>
-                                    <Form.Control type="password" />
+                                    <Form.Control onChange={this.onChange}
+                                        value={this.state.password}
+                                        error={errors.password}
+                                        id="password"
+                                        type="password" />
                                 </FormGroup>
                             </Col>
                             <Col>
                                 <FormGroup>
                                     <Form.Label>Confirm Password</Form.Label>
-                                    <Form.Control type="password" />
+                                    <Form.Control
+                                        onChange={this.onChange}
+                                        value={this.state.password2}
+                                        error={errors.password2}
+                                        id="password2"
+                                        type="password" />
                                 </FormGroup>
                             </Col>
-                            <Button size="lg" block className="mt-4" >Register</Button>
+                            <Button size="lg" block className="mt-4" type="submit" >Register</Button>
                             <div className="text-center mt-2">
                                 <p> Already have an account? <a href="/login">Sign in</a></p>
                             </div>
